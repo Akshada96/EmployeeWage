89241569b4c411bbe744a499bea9bd1d2b1abc02
Akshada <akshumore96@gmail.com>
2022-03-15 01:11:20 +0530

    Added UC-2

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,13 +2,12 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int random = (int) (Math.random() * 2);
+        int wagePerHr = 20;
+        int salary = 0;
         if (random == 1) {
-            System.out.println("Employee is present");
+            salary = 20*8;
         }
-        else {
-            System.out.println("Employee is absent");
-        }
+        System.out.println("Employee daily wage is "+salary);
     }
 }
